fc10997049bd703d87eadb56cdfb782ae27f6b44
BlakWeb <144697255+BlakWeb@users.noreply.github.com>
2024-08-10 01:10:11 +0100

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,18 +8,18 @@ const Hero = () => {
         <div id="home" className="hero">
             <img src={profile_img} alt="Profile" />
             <h1>
-                <span>I'm Jacob O. Yahaya,</span> frontend web developer based in Nigeria
+                <span>I'm Oshala Jacob Yahaya,</span> frontend web developer based in Nigeria.
       </h1>
             <p>
                 I am a passionate front-end web developer from Nasarawa State, Nigeria
-                with 3 years of expertise in web development.
+                with 1+ years of expertise in web development.
       </p>
             <div className="hero-action">
                 <div className="hero-connect">
                     <AnchorLink className="anchor-link" offset={50} href="#contact">Connect with me</AnchorLink>
                 </div>
                 <div className="hero-resume">
-                    <a href="https://drive.google.com/file/d/1HOkahwCsz0jtGrkimwjAGf2WjuAWPCnU/view?fbclid=IwY2xjawEc3gRleHRuA2FlbQIxMAABHT8zV9tAqHGkRPEMQrSh3SfRSCrvXrI4E_MflpBvc9lPsFndaEqjjWbrmQ_aem_gnMD_H2_yI6soZEAJQKh3w"> Check my resume</a>
+                    <a href="https://drive.google.com/file/d/1uvXI8oA00wqzNpzxS-buAjti69YfPHTg/view?fbclid=IwY2xjawEjiHRleHRuA2FlbQIxMQABHefHaDIa7AkD19EgzkTlZeQ_7fPm27l0nKSpBorN1TwfsIp2ZIvt3D5rrA_aem_e-h-2pF6q3fJkbMszhw5xA"> Check my resume</a>
                 </div>
             </div>
         </div>
