4f40f579869afd081a52dbd380459a07077fe809
BlakWeb <144697255+BlakWeb@users.noreply.github.com>
2024-08-10 01:15:47 +0100

    Update services_data.js

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -2,32 +2,32 @@ const Services_Data = [
   {
     s_no: "01",
     s_name: "Web development",
-    s_desc: "Web development is the process of building, programming...",
+    s_desc: "Web development refers to the tasks associated with creating, building, and maintaining websites and web applications...",
   },
   {
     s_no: "02",
     s_name: "Freelancer",
-    s_desc: "Web development is the process of building, programming...",
+    s_desc: "A freelancer is a person who is self-employed, often working for multiple clients at one time and earning income per project...",
   },
   {
     s_no: "03",
-    s_name: "Social media",
-    s_desc: "Web development is the process of building, programming...",
+    s_name: "Social media marketing",
+    s_desc: "Social media marketing (SMM) is the use of social media platforms to interact with customers to build brands, increase sales...",
   },
   {
     s_no: "04",
     s_name: "App design",
-    s_desc: "Web development is the process of building, programming...",
+    s_desc: "In the context of app development, app design is the process of creating the visual look, layout, and flow of an application's interface...",
   },
   {
     s_no: "05",
     s_name: "Digital marketing",
-    s_desc: "Web development is the process of building, programming...",
+    s_desc: "Digital marketing is a means of promoting and selling products and services through the internet, mobile devices, social media...",
   },
   {
     s_no: "06",
     s_name: "Content writing",
-    s_desc: "Web development is the process of building, programming...",
+    s_desc: "Content writing is the process of planning, writing, and publishing written material, such as articles, blog post...",
   },
 ];
 
