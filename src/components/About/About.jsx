f2f8d6be321d3d68eb739bcfb41a99475f900348
BlakWeb <144697255+BlakWeb@users.noreply.github.com>
2025-07-28 01:09:32 +0100

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,12 +17,12 @@ const About = () => {
         <div className="about-right">
           <div className="about-para">
             <p>
-              I am a skilled front end developer with 1+ years of experience
+              I am a skilled Front-End and Wordpress Web Developer with 2+ years of experience
               in creating user-friendly, responsive websites.
             </p>
             <p>
               A goal-oriented Frontend Developer with knowledge of HTML, CSS,
-              JavaScript, React, Nextjs, Tailwind and Typescript seeking to use
+              JavaScript, React, Nextjs, Tailwind, Typescript and Wordpress seeking to use
               technical proficiency and creativity to develop engaging user
               experiences and advance in a professional career.
             </p>
@@ -38,11 +38,11 @@ const About = () => {
             </div>
             <div className="about-skill">
               <p>React JS</p>
-              <hr style={{ width: "55%" }} />
+              <hr style={{ width: "70%" }} />
             </div>
             <div className="about-skill">
-              <p>Next JS</p>
-              <hr style={{ width: "45%" }} />
+              <p>NextJS</p>
+              <hr style={{ width: "50%" }} />
             </div>
             <div className="about-skill">
               <p>Typescript</p>
@@ -50,24 +50,28 @@ const About = () => {
             </div>
             <div className="about-skill">
               <p>Tailwind</p>
-              <hr style={{ width: "53%" }} />
+              <hr style={{ width: "55%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Wordpress</p>
+              <hr style={{ width: "55%" }} />
             </div>
           </div>
         </div>
       </div>
       <div className="about-achievements">
         <div className="about-achievement">
-          <h1>1+</h1>
+          <h1>2+</h1>
           <p>YEARS OF EXPERIENCE</p>
         </div>
         <hr />
         <div className="about-achievement">
-          <h1>20+</h1>
+          <h1>22+</h1>
           <p>PROJECTS COMPLETED</p>
         </div>
         <hr />
         <div className="about-achievement">
-          <h1>5+</h1>
+          <h1>8+</h1>
           <p>HAPPY CLIENTS</p>
         </div>
       </div>
